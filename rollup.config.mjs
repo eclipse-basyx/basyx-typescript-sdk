e06f048eeb1854ba698842131681d6ba077073c0
Aaron Zielstorff <aaron.zielstorff@iese.fraunhofer.de>
2025-02-07 22:42:08 +0100

    Adds rollup bundler (#36)

---
+++ rollup.config.mjs
@@ -0,0 +1,18 @@
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+
+export default {
+    // Use your compiled entry point from the dist folder
+    input: 'dist/index.js',
+    output: {
+        file: 'bundle/index.js',
+        format: 'cjs', // CommonJS module format
+        sourcemap: true,
+    },
+    plugins: [
+        resolve(), // helps Rollup find node_modules packages
+        commonjs(), // converts CommonJS modules to ES6, so they can be included in a Rollup bundle
+    ],
+    // Externalize dependencies that shouldn't be bundled
+    external: ['@aas-core-works/aas-core3.0-typescript', '@hey-api/client-fetch'],
+};
