7e5bd8ba28bd9cd956dc0153e9197de4af39062b
Anuja Chakraborty <anujac234@gmail.com>
2025-08-05 22:15:05 +0200

    added a testcase for AasxFileServer's integration test

--- src/integration-tests/aasxFile.integration.test.ts
+++ src/integration-tests/aasxFile.integration.test.ts
@@ -1,10 +1,10 @@
-// import fs from 'fs';
-// import { AasxFileClient } from '../clients/AasxFileClient';
-// import { AasxFileService } from '../generated';
-// import { createTestShell } from './fixtures/aasxFileFixtures';
+import { AasxFileClient } from '../clients/AasxFileClient';
+import { AasxFileService } from '../generated';
+//import { createTestShell } from './fixtures/aasxFileFixtures';
+//import fs from 'fs';
 
 describe('AASX File Server Integration Tests', () => {
-    // const client = new AasxFileClient();
+    const client = new AasxFileClient();
     // const testShell = createTestShell();
     // //const testPackageDescription = createTestPackageDescription();
     // const fileName = 'sample.aasx';
@@ -13,9 +13,11 @@ describe('AASX File Server Integration Tests', () => {
     // });
     // //const packageId = 'aasx-package-01';
     // let createdPackageId: string;
-    // const configuration = new AasxFileService.Configuration({
-    //     basePath: 'http://localhost:8087',
-    // });
+
+    const configuration = new AasxFileService.Configuration({
+        basePath: 'http://localhost:8087',
+    });
+
     // test('should store the AASX package at the server', async () => {
     //     const response = await client.postAASXPackage({
     //         configuration,
@@ -23,6 +25,7 @@ describe('AASX File Server Integration Tests', () => {
     //         fileName,
     //         file,
     //     });
+
     //     expect(response.success).toBe(true);
     //     if (response.success) {
     //         expect(response.data).toBeDefined();
@@ -31,29 +34,33 @@ describe('AASX File Server Integration Tests', () => {
     //         //expect(response.data).toEqual(testPackageDescription);
     //     }
     // });
+
     // test('should fetch a specific AASX package from the server', async () => {
     //     const response = await client.getAASXByPackageId({
     //         configuration,
     //         packageId: createdPackageId,
     //     });
+
     //     expect(response.success).toBe(true);
     //     if (response.success) {
     //         expect(response.data).toBeDefined();
     //         //expect(response.data).toEqual(file);
     //     }
     // });
-    // test('should fetch a specific AASX package by non-existing ID', async () => {
-    //     const nonExistingId = 'non-existing-id';
-    //     const response = await client.getAASXByPackageId({
-    //         configuration,
-    //         packageId: nonExistingId,
-    //     });
-    //     expect(response.success).toBe(false);
-    //     if (!response.success) {
-    //         expect(response.error).toBeDefined();
-    //         console.log('Error:', response.error);
-    //     }
-    // });
+
+    test('should fetch a specific AASX package by non-existing ID', async () => {
+        const nonExistingId = 'non-existing-id';
+        const response = await client.getAASXByPackageId({
+            configuration,
+            packageId: nonExistingId,
+        });
+        expect(response.success).toBe(false);
+        if (!response.success) {
+            expect(response.error).toBeDefined();
+            console.log('Error:', response.error);
+        }
+    });
+
     // test('should update the AASX package at the server', async () => {
     //     const updateResponse = await client.putAASXByPackageId({
     //         configuration,
@@ -62,26 +69,33 @@ describe('AASX File Server Integration Tests', () => {
     //         fileName,
     //         file,
     //     });
+
     //     console.log('Update Response:', updateResponse);
+
     //     expect(updateResponse.success).toBe(true);
+
     //     const fetchResponse = await client.getAASXByPackageId({
     //         configuration,
     //         packageId: createdPackageId,
     //     });
+
     //     expect(fetchResponse.success).toBe(true);
     //     if (fetchResponse.success) {
     //         expect(fetchResponse.data).toBeDefined();
     //         //expect(fetchResponse.data).toEqual(file);
     //     }
     // });
+
     // test('should fetch a list of available AASX packages', async () => {
     //     const response = await client.getAllAASXPackageIds({
     //         configuration,
     //     });
+
     //     expect(response.success).toBe(true);
     //     if (response.success) {
     //         expect(response.data).toBeDefined();
     //         expect(response.data.result).toBeDefined();
+
     //         const serverResponse = response.data.result as any;
     //         expect(serverResponse.result).toBeDefined();
     //         expect(Array.isArray(serverResponse.result)).toBe(true);
