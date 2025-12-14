8fd580db5107bdd56624adfb59d23936312db9e4
Aaron Zielstorff <aaron.zielstorff@iese.fraunhofer.de>
2025-12-14 17:30:40 +0100

    Exports utils functions

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from './clients';
 export * from './generated';
 export * as Lib from './lib';
 export * as Models from './models';
+export * as Utils from './utils';
---
+++ src/utils/index.ts
@@ -0,0 +1,15 @@
+// Re-export all utilities as named exports for direct usage
+export * from './DescriptorUtils';
+export * from './KeyTypesUtil';
+export * from './MultiLanguagePropertyUtils';
+export * from './ReferableUtils';
+export * from './ReferenceUtil';
+export * from './SemanticIdUtils';
+
+// Also export as namespaces for organized imports
+export * as DescriptorUtils from './DescriptorUtils';
+export * as KeyTypesUtil from './KeyTypesUtil';
+export * as MultiLanguagePropertyUtils from './MultiLanguagePropertyUtils';
+export * as ReferableUtils from './ReferableUtils';
+export * as ReferenceUtil from './ReferenceUtil';
+export * as SemanticIdUtils from './SemanticIdUtils';
